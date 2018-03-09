948ca90e3eb0ea57ef75cc0e4b0c7b6e82ae8c41
chenmich <403189920@qq.com>
2018-03-09 16:05:11 +0800

    complete the quick introduction of pandas

---
+++ quick_pandas.py
@@ -0,0 +1,28 @@
+import pandas as pd
+print(pd.__version__)
+city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+population = pd.Series([852469, 1015785, 485199])
+#city_population_table = pd.DataFrame(({'City name': city_names, 'Population': population}))
+california_houseing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+california_houseing_dataframe.describe()
+california_houseing_dataframe.head()
+#some error
+#california_houseing_dataframe.hist('housing_median_age')
+cities = pd.DataFrame({'City name': city_names, 'Population': population})
+#print(type(cities['City name']))
+#print(cities['City name'])
+#print(type(cities['City name'][1]))
+#print(cities['City name'][1])
+#print(type(cities[0:2]))
+#print(cities[0:2])
+#print(population / 1000)
+import numpy as np
+np.log(population)
+#print(population.apply(lambda val: val > 10000))
+cities['Area square miles'] = pd.Series([46.87, 176.53, 97.92])
+#print(cities)
+cities['Population density'] = cities['Population'] / cities['Area square miles']
+#print(cities)
+print(city_names.index)
+print(cities.reindex([2, 0, 1]))
+print(cities)
